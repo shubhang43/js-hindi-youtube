ba5e23cde7c9213f9bcc9de4cd2af6f95f94f385
Shubhang Deep Singh <167906775+shubhang43@users.noreply.github.com>
2026-06-09 13:38:19 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,5 +24,47 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ******** Operations ********
+
+let value =  3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = " shubhang"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); // if string is first then others will also be treated like string ("")
+// console.log(1 + 2 + "2");//if string is in the last then conversion will be done
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);// don't do all these kind of converison always keep it simple
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html
+// also study about prefix and postfix on mdn
