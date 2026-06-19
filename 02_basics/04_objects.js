6640baf79fecfabdfc9c12bdace6fe749c612f2b
Shubhang Deep Singh <167906775+shubhang43@users.noreply.github.com>
2026-06-19 14:09:41 +0000

    json format talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -51,11 +51,35 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+// console.log(tinderUser.hasOwnProperty('isLogged'));
 
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "shubhang"
+}
+
+// course.courseInstructor
+
+const  {courseInstructor: instructor} = course // destructuring
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//    "name": "shubhang"
+//    "coursename": "js in hindi"
+//    "price": "free"
+// } // json format
+
+[
+    {},
+    {},
+    {},
+]
